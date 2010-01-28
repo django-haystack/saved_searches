a44faf18b5ab62a5a6aeb749d88f279e97f9e7dd
Daniel Lindsley <daniel@toastdriven.com>
2010-01-28 01:52:56 -0600

    Copy-paste foul in ``setup.py``. v1.0.1!

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 
 setup(
     name='saved_searches',
-    version='1.0.0',
+    version='1.0.1',
     description='Saves user searches for integration with Haystack.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
     url='http://github.com/toastdriven/saved_searches',
     packages=[
         'saved_searches',
-        'queued_search.templatetags',
+        'saved_searches.templatetags',
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
